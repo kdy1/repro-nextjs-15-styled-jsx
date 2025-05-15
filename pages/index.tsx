286ef14d2f4864830ea30e555974818acea7849c
Victor Pontis <victor@pont.is>
2025-05-15 12:27:51 -0400

    Add global style example

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,21 @@ export default function Home() {
     <>
       <div className="container">
         container (should be blue)
+
         <div className="child">
           this should be green
         </div>
       </div>
 
+
       <style jsx>{`
           .container {
               color: blue;
               padding: 3rem;
+          }
 
-              :global(div.child) {
-                  color: green;
-              }
+          :global(div.child) {
+              color: green;
           }
       `}</style>
     </>
