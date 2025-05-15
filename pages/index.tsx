e5a662a16849bd7058bbabcfb483b57c9337f9ba
Victor Pontis <victor@pont.is>
2025-05-15 12:20:59 -0400

    Remove responsive styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 export default function Home() {
-  const breakpoint = '500px'
   return (
     <>
       <div className="container">
         container (should be blue)
-        <div className="responsive">
-          responsive (purple on mobile, orange on desktop)
-        </div>
-
         <div className="child">
           this should be green
         </div>
@@ -17,16 +12,6 @@ export default function Home() {
           .container {
               color: blue;
               padding: 3rem;
-
-              .responsive {
-                  color: orange;
-              }
-
-              @media (max-width: ${breakpoint}) {
-                  .responsive {
-                      color: purple;
-                  }
-              }
 
               :global(div.child) {
                   color: green;
