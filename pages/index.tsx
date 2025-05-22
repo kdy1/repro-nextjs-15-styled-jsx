b215e87aadaf0060e6f13f81dba338642dd13eb3
Victor Pontis <victor@pont.is>
2025-05-22 17:10:49 -0400

    Upgrade Nextjs version

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,19 @@ export default function Home() {
     <>
       <div className="container">
         container (should be blue)
-
-        <div className="child">
-          this should be green
-        </div>
+        <div className="child">this should be green</div>
       </div>
 
-
       <style jsx>{`
-          .container {
-              color: blue;
-              padding: 3rem;
-          }
+          div {
+              &.container {
+                  color: blue;
+                  padding: 3rem;
 
-          :global(div.child) {
-              color: green;
+                  .child {
+                      color: green;
+                  }
+              }
           }
       `}</style>
     </>
